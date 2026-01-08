1ced4e9e732f8a8acb5da934f122aaac2948f1cd
Carlos <carlosaac232001@gmail.com>
2026-01-08 08:42:35 -0500

    chore: import new component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import RenderName from './RenderName.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <RenderName name='Carlos' />
+  </StrictMode>
+);
